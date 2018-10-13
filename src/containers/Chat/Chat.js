07b9333f56f5fcbf56d7fa8c6ac4f590864002aa
FernanityChamp <andres.fernando08@gmail.com>
2018-10-13 18:27:55 -0500

    desde el componente Chat es invocado los componentes de interaccion con el usuario

--- src/containers/Chat/Chat.js
+++ src/containers/Chat/Chat.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import Styles from './Chat.module.css';
+
+import HistoricalMessage from '../../components/HistoricalMessage/HistoricalMessage';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+
+
 class Chat extends Component {
+    
+
     render() {
         return (
-            <div>
-
+            <div className={Styles.Chat}>
+                <HistoricalMessage />
+                <div className={Styles.UserInput}>
+                    <Input />
+                    <Button>Enviar</Button>
+                </div>
             </div>
         )
     }
