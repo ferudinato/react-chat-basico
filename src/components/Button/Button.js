b8e06c993e57eba82b8f9ef3de5e7884e4e5168e
FernanityChamp <andres.fernando08@gmail.com>
2018-10-13 18:26:11 -0500

    se crearon los componentes para que el usuario interacture con el chat, con sus respectivos estilos css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Styles from './Button.module.css';
+
 const button = (props) => (
-    <button type={props.btnType} onClick={props.clicked}>{props.children}</button>
+    <button 
+        type={props.btnType} 
+        onClick={props.clicked}
+        className={Styles.Button}>{props.children}</button>
 )
 
 export default button;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Styles from './Input.module.css';
+
 const input = (props) => (
     <input 
         type={props.inputType} 
         placeholder={props.placeholder}
         onChange={props.changed} 
-        value={props.val} />
+        value={props.val}
+        className={Styles.Input} />
 )
 
 export default input;
